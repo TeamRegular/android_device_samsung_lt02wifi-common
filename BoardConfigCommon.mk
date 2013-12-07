b63d7a05448d156e0e0bcbfd90f226dd1644d4d5
C457 <android.c357@gmail.com>
2013-12-07 03:50:16 -0600

    Clean up for common repos

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -13,12 +13,15 @@
 # limitations under the License.
 #
 
+# inherit from common pxa988
+-include device/samsung/pxa988-common/BoardConfigCommon.mk
+
 BOARD_VENDOR := samsung
+
 TARGET_BOARD_PLATFORM := mrvl
-TARGET_NO_BOOTLOADER := true
 TARGET_NO_RADIOIMAGE := true
 ARCH_ARM_HAVE_TLS_REGISTER := true
-TARGET_BOOTLOADER_BOARD_NAME := PXA986
+TARGET_BOOTLOADER_BOARD_NAME := PXA988
 
 # Kernel
 TARGET_KERNEL_SOURCE         := kernel/samsung/lt02wifi-common-common
@@ -35,16 +38,6 @@ BOARD_HAS_NO_MISC_PARTITION := true
 BOARD_HAS_NO_SELECT_BUTTON := true
 TARGET_RECOVERY_FSTAB := device/samsung/lt02wifi-common/rootdir/etc/recovery.fstab
 
-TARGET_ARCH := arm
-TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_CPU_VARIANT := cortex-a9
-TARGET_CPU_SMP := true
-TARGET_CPU_ABI := armeabi-v7a
-TARGET_CPU_ABI2 := armeabi
-ARCH_ARM_HAVE_NEON := true
-TARGET_GLOBAL_CFLAGS += -mtune=cortex-a9 -mfpu=neon -mfloat-abi=softfp
-TARGET_GLOBAL_CPPFLAGS += -mtune=cortex-a9 -mfpu=neon -mfloat-abi=softfp
-
 TARGET_USERIMAGES_USE_EXT4 := true
 BOARD_BOOTIMAGE_PARTITION_SIZE := 12582912
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 12582912
@@ -52,41 +45,6 @@ BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1507852288
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 5775556608
 BOARD_FLASH_BLOCK_SIZE := 131072
 
-# Wifi related defines
-BOARD_WLAN_DEVICE := mrvl
-WPA_SUPPLICANT_VERSION        := VER_0_8_X
-BOARD_WPA_SUPPLICANT_DRIVER := NL80211
-BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_mrvl8787
-WIFI_DRIVER_FW_PATH_STA        := "/system/etc/firmware/sd8787_uapsta.bin"
-
-# Vold
-BOARD_VOLD_MAX_PARTITIONS := 17
-BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
-BOARD_VOLD_DISC_HAS_MULTIPLE_MAJORS := true
-TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/class/android_usb/f_mass_storage/lun%d/file"
-
-# Graphics
-BOARD_EGL_CFG := device/samsung/lt02wifi-common/configs/egl.cfg
-USE_OPENGL_RENDERER := true
-
-# Audio
-BOARD_USES_ALSA_AUDIO := true
-
-# Webkit
-ENABLE_WEBGL := true
-TARGET_FORCE_CPU_UPLOAD := true
-
-# Power
-TARGET_PROVIDES_POWERHAL := true
-
 # Bluetooth
-BOARD_HAVE_BLUETOOTH := true
-BOARD_HAVE_BLUETOOTH_BCM := true
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/lt02wifi-common/bluetooth
-
-# Charging mode
-BOARD_CHARGING_MODE_BOOTING_LPM := /sys/class/power_supply/battery/batt_lp_charging
-BOARD_BATTERY_DEVICE_NAME := "battery"
-
-#TARGET_SPECIFIC_HEADER_PATH := device/samsung/lt02wifi-common/include
 
--- lt02wifi-common.mk
+++ lt02wifi-common.mk
@@ -25,7 +25,6 @@ PRODUCT_PACKAGES += \
     fstab.pxa988 \
     init.pxa988.rc \
     init.pxa988.usb.rc \
-    lpm.rc \
     ueventd.pxa988.rc
 
 # Recovery rootdir
@@ -64,58 +63,23 @@ PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/asound.conf:system/etc/asound.conf \
     $(COMMON_PATH)/configs/audio_effects.conf:system/etc/audio_effects.conf \
 
-# Audio
-PRODUCT_PACKAGES += \
-    audio.a2dp.default \
-    audio.usb.default
-
 # inherit alsa-lib-products.mk
 $(call inherit-product, device/samsung/lt02wifi-common/alsa-lib/alsa-lib-products.mk)
-
-# Charger
-PRODUCT_PACKAGES += \
-    charger \
-    charger_res_images
 
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml \
-    frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
-    frameworks/native/data/etc/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml \
-    frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
-    frameworks/native/data/etc/android.hardware.location.xml:system/etc/permissions/android.hardware.location.xml \
-    frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
-    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
-    frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
-    frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
-    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
-    frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
-    frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
-    frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
-    frameworks/native/data/etc/android.hardware.touchscreen.xml:system/etc/permissions/android.hardware.touchscreen.xml \
-    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
-    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
-    frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
-    frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
-    packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml
 
 # media config xml file
 PRODUCT_COPY_FILES += \
     device/samsung/lt02wifi-common/configs/media_profiles.xml:system/etc/media_profiles.xml \
     device/samsung/lt02wifi-common/configs/media_profiles.xml_for5640:system/etc/media_profiles.xml_for5640
 
-# media codec config xml file
-PRODUCT_COPY_FILES += \
-    device/samsung/lt02wifi-common/configs/media_codecs.xml:system/etc/media_codecs.xml
-
 PRODUCT_CHARACTERISTICS := tablet
 
 PRODUCT_PACKAGES += \
     libxml2 \
-    setup_fs \
-    hcitool \
-    librs_jni \
-    com.android.future.usb.accessory
+    hcitool
 
 # Enable repeatable keys in cwm
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -126,7 +90,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.opengles.version=131072
 
-PRODUCT_TAGS += dalvik.gc.type-precise
+# call common pxa988
+$(call inherit-product, device/samsung/pxa988-common/pxa988.mk)
+
 
 $(call inherit-product, frameworks/native/build/tablet-7in-hdpi-1024-dalvik-heap.mk)
 $(call inherit-product-if-exists, vendor/samsung/lt02wifi-common/lt02wifi-common-vendor.mk)
